4ac39ab6422ef2eadfa4c9fd6d6ff845fd6a4465
poorvakhatri04 <146342264+poorvakhatri04@users.noreply.github.com>
2025-07-12 21:15:52 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId=14453
+// accountId=2
+console.log(accountId);
+let accountEmail="pk@gooogle.com"
+accountEmail="ds@gmail.com"
+let accState
+console.table([accountEmail,accountId,accState])
+/* Dont use var because of issues in block scope and functional scope*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict";//treat all code as newer version
+//alert(3+3)//we are using nodejs, not browser
+let name="poorva"
+let age=18
+let isloggedin=false
+//number=>2^53
+//bigint
+//string=>""
+//boolean=>true/flase
+//null=>standalone value, empty value
+//undefined=> value not assigned
+//symbol=> used to find uniqueness
+//object
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
