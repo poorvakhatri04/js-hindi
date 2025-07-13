a6e19d6fd70ab9837adbbaa167409b0949ceb943
poorvakhatri04 <146342264+poorvakhatri04@users.noreply.github.com>
2025-07-13 06:37:16 +0000

    Added JS conversion operations example

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,25 @@
+let score="4"
+console.log(typeof score)
+let val1=Number(score)
+console.log(typeof val1)
+//"33"=> 33
+//"33abc"=>NaN
+//true=>1
+//false=> 0
+//null=>0
+//undefined=>NaN
+
+//Boolean conversion
+//1=>true
+//0=>false
+//""=>false
+//"pua"=> true
+
+// ********operations******
+let val=3;
+let negval=-val;
+console.log(negval)
+console.log("1"+2+2)//122
+console.log(1+2+"2")//32
+console.log(+true)//1
+console.log(+"")//0
